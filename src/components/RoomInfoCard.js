871dff78d24cee63604b845ff6eb889b004d81d3
benhlal <benhlalyounes@gmail.com>
2021-06-08 18:33:59 +0200

    [chc-v0] room info feed

---
+++ src/components/RoomInfoCard.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import style from '../style/roomCard.module.css'
+import data from '../data/roomCard.json'
+import Avatar from '@material-ui/core/Avatar';
+import {makeStyles} from "@material-ui/core";
+import {BsChatDots, BsChatDotsFill, BsFillPersonFill} from "react-icons/all";
+
+
+function RoomInfoCard(props) {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+        small: {
+            width: theme.spacing(3),
+            height: theme.spacing(3),
+        },
+        large: {
+            width: theme.spacing(7),
+            height: theme.spacing(7),
+        },
+    }));
+
+    const classes = useStyles();
+
+    return (
+        <>{
+            data.map((item) => (
+                <div>
+                    <div>
+                        <div className={style.roomCardContainer}>
+                            <h6>{item.title}</h6>
+                            <h2>{item.sub_title}</h2>
+
+                            <div className={style.roomMembers}>
+                                <div>
+                                    <img alt="Remy Sharp" src="/images/b2.jpg" className={classes.small}/>
+                                    <img alt="Remy Sharp" src="/images/pro.png"/>
+                                </div>
+                                <div>
+                                    {item.members.map((member) => (
+                                        <p>
+                                            {member.first_name} {member.last_name} <BsChatDots/>
+                                        </p>
+                                    ))}
+                                    <p className="d-flex align-items-center">
+                                        <span className="mx-2">1.8</span>
+                                        <BsFillPersonFill/>
+                                        <span className="mx-2"></span>{" "}
+                                        <span className="mx-2">5</span> <BsChatDotsFill/>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            ))}</>
+
+
+    );
+}
+
+export default RoomInfoCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import Header from "../components/Header";
+import DailyCard from "../components/DailyCard";
+import style from '../style/home.module.css'
+import RoomInfoCard from "../components/RoomInfoCard";
+import {AiOutlinePlus, AiOutlinePushpin, BsGrid3X3Gap} from "react-icons/all";
 
 function Home(props) {
-    return (
-        <Header/>
-    );
+    return (<>
+            <Header/>
+            <div className={style.homeCardContainer}>
+                <DailyCard/>
+                <RoomInfoCard/>
+            </div>
+
+            <div className={style.actionBtn}>
+                <button>
+                    <AiOutlinePlus style={{marginRight: '0.500em'}}/>
+                    Start a room
+                </button>
+                <button>
+                    <BsGrid3X3Gap style={{marginRight: '0.500em'}}/>
+                </button>
+            </div>
+        </>
+
+    )
+        ;
 }
 
 export default Home;
